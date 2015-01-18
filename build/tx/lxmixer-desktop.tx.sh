0a537669f475faeb1a43b89c3b4c216ca6452cbc
Kinokoio <kinokoio@hushmail.com>
2015-01-18 12:24:34 +0000

    Erased build/ directory. It was causing problems at build.

--- build/tx/lxmixer-desktop.tx.sh
+++
@@ -1,7 +0,0 @@
-[ -f /home/pranav/Github-Projects/lxmixer/resources/lxmixer-desktop.in ] || exit 0
-echo '[lxde-qt.lxmixer-desktop_]'
-echo 'type = DESKTOP'
-echo 'source_lang = en'
-echo 'source_file = resources/lxmixer-desktop.in'
-echo 'file_filter = translations/lxmixer-desktop_<lang>'
-echo ''
--- build/tx/lxqt-about.desktop.tx.sh
+++
@@ -1,7 +0,0 @@
-[ -f /home/pranav/Github-Projects/lxmixer/resources/lxqt-about.desktop.in ] || exit 0
-echo '[lxde-qt.lxqt-about_desktop]'
-echo 'type = DESKTOP'
-echo 'source_lang = en'
-echo 'source_file = resources/lxqt-about.desktop.in'
-echo 'file_filter = translations/lxqt-about_<lang>.desktop'
-echo ''
