b37a672c0a87380936d5757d6f6912967434dcad
Pranav Ganorkar <pranavg189@gmail.com>
2015-01-21 20:28:13 +0530

    cleaned source code for kdevelop

--- build/tx/lxmixer.desktop.tx.sh
+++
@@ -1,7 +0,0 @@
-[ -f /home/pranav/Github-Projects/lxmixer/resources/lxmixer.desktop.in ] || exit 0
-echo '[lxde-qt.lxmixer_desktop]'
-echo 'type = DESKTOP'
-echo 'source_lang = en'
-echo 'source_file = resources/lxmixer.desktop.in'
-echo 'file_filter = translations/lxmixer_<lang>.desktop'
-echo ''
